712965dd3362b983b3822f42fdf353a4f6e89354
DennerOl <dener155@hotmail.com>
2023-10-14 22:01:32 -0300

    Preparando o projeto para testes OAuth2 e validação

--- src/test/java/com/devsuperior/demo/controllers/DepartmentControllerIT.java
+++ src/test/java/com/devsuperior/demo/controllers/DepartmentControllerIT.java
@@ -4,6 +4,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,6 +14,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.demo.tests.TokenUtil;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
@@ -20,6 +23,65 @@ public class DepartmentControllerIT {
 
 	@Autowired
 	private MockMvc mockMvc;
+	
+	@Autowired
+	private TokenUtil tokenUtil;
+	
+	private String operatorUsername;
+	private String operatorPassword;
+	private String adminUsername;
+	private String adminPassword;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		operatorUsername = "ana@gmail.com";
+		operatorPassword = "123456";
+		adminUsername = "bob@gmail.com";
+		adminPassword = "123456";
+	}
+	
+	@Test
+	public void findAllShouldReturnAllResourcesSortedByNameWhenAdminLogged() throws Exception {
+		
+		String accessToken = tokenUtil.obtainAccessToken(mockMvc, adminUsername, adminPassword);
+		
+		ResultActions result =
+				mockMvc.perform(get("/departments")
+					.header("Authorization", "Bearer " + accessToken)
+					.contentType(MediaType.APPLICATION_JSON));
+
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$[0].name").value("Management"));
+		result.andExpect(jsonPath("$[1].name").value("Sales"));
+		result.andExpect(jsonPath("$[2].name").value("Training"));
+	}
+	
+	@Test
+	public void findAllShouldReturnAllResourcesSortedByNameWhenEmployeeLogged() throws Exception {
+		
+		String accessToken = tokenUtil.obtainAccessToken(mockMvc, operatorUsername, operatorPassword);
+		
+		ResultActions result =
+				mockMvc.perform(get("/departments")
+					.header("Authorization", "Bearer " + accessToken)
+					.contentType(MediaType.APPLICATION_JSON));
+
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$[0].name").value("Management"));
+		result.andExpect(jsonPath("$[1].name").value("Sales"));
+		result.andExpect(jsonPath("$[2].name").value("Training"));
+	}
+	
+	@Test
+	public void findAllShouldReturn401WhenNoUserLogged() throws Exception {
+		
+		ResultActions result =
+				mockMvc.perform(get("/departments")
+					.contentType(MediaType.APPLICATION_JSON));
+
+		result.andExpect(status().isUnauthorized());
+	}
 	
 	@Test
 	public void findAllShouldReturnAllResourcesSortedByName() throws Exception {
